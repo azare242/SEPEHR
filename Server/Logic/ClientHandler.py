cd65ad2fd8c185fa5427259ded10581afe48e41c
Alireza Zare <Azare242@gmail.com>
2022-06-05 18:49:08 +0430

    FEAT : Server Implementation Started

---
+++ Server/Logic/ClientHandler.py
@@ -0,0 +1,34 @@
+import threading
+from threading import *
+import socket
+from Server.Logic.Server import Server
+from Server.Model.User import User
+
+
+class ClientHandler:
+    def __init__(self, client_socket: socket.socket, server: Server):
+        self.user = None
+        self.client_socket = client_socket
+        self.thread = threading.Thread(target=self.handling, args=(socket,))
+        self.server = server
+
+    def generate_user(self, info):
+        return User(info[0], info[1], info[2], info[3], info[4])
+
+    def handling(self):
+        try:
+            self.client_socket.send('login'.encode('utf-8'))
+            while True:
+                user_information = self.client_socket.recv(4096).decode('utf-8')
+                info = user_information.split('//')
+                if self.server.client_handlers.search_by_username(info[0]) is not None:
+                    self.user = self.generate_user(info)
+                    self.client_socket.send('done'.encode('utf-8'))
+                else:
+                    self.client_socket.send('try-again'.encode('utf-8'))
+        except socket.error as err:
+            self.client_socket.close()
+            self.server.client_handlers.remove_client(self)
+
+    def start(self):
+        self.thread.start()
---
+++ Server/Logic/Server.py
@@ -0,0 +1,37 @@
+import socket
+import _thread
+from Utils import Properties
+from Server.Model import Sets
+from Server.Logic.ClientHandler import ClientHandler
+
+
+
+class Server:
+    def __init__(self):
+        self.client_handlers = Sets.ClientHandlersSet()
+        self.HOST = Properties.get('Server', "HOST")
+        self.PORT = Properties.get('Server', "HOST")
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.HOST, self.PORT))
+        self.socket.listen()
+
+
+
+    def login(self, client: socket.socket):
+        client.send('login'.encode('utf-8'))
+        while True:
+            user_information = client.recv(4096).decode('utf-8')
+            info = user_information.split('//')
+            if self.client_handlers.search_by_username(info[0]) is not None:
+                user = self.generate_user(info)
+                client.send('done'.encode('utf-8'))
+            else:
+                client.send('try-again'.encode('utf-8'))
+
+    def server_loop(self):
+        while True:
+            client, address = self.socket.accept()
+            print(f'Connected with {str(address)}')
+            client_handler = ClientHandler(client, self)
+            client_handler.start()
