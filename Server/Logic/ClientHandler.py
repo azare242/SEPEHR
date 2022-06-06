6ab5388792cca07cda26ac7b984e45edace8bc97
Alireza Zare <Azare242@gmail.com>
2022-06-06 14:02:16 +0430

    FEAT : Client disconnect and end program handled in server side

--- Server/Logic/ClientHandler.py
+++ Server/Logic/ClientHandler.py
@@ -2,6 +2,11 @@ import threading
 from threading import *
 import socket
 from Server.Logic.Server import Server
+from Server.Model.User import User
+
+
+def generate_user(info):
+    return User(info[0], info[1], info[2], info[3], info[4])
 
 
 class ClientHandler:
@@ -11,19 +16,23 @@ class ClientHandler:
         self.thread = threading.Thread(target=self.handling, args=(socket,))
         self.server = server
 
-
     def handling(self):
         try:
             self.client_socket.send('login'.encode('utf-8'))
             while True:
                 user_information = self.client_socket.recv(4096).decode('utf-8')
                 info = user_information.split('//')
-                if self.server.client_handlers.search_by_username(info[0]) is not None:
-                    self.user = self.generate_user(info)
+                if self.server.client_handlers.search_by_username(info[0]) is None:
+                    self.user = generate_user(info)
                     self.client_socket.send('done'.encode('utf-8'))
+                    break
                 else:
                     self.client_socket.send('try-again'.encode('utf-8'))
-        except socket.error as err:
+
+        except socket.error as err :
+            self.client_socket.close()
+            self.server.client_handlers.remove_client(self)
+        finally:
             self.client_socket.close()
             self.server.client_handlers.remove_client(self)
 
--- Server/Logic/Server.py
+++ Server/Logic/Server.py
@@ -1,11 +1,10 @@
 import socket
 import _thread
 
-from Server.Model.User import User
+
 from Utils import Properties
 from Server.Model import Sets
 from Server.Logic.ClientHandler import ClientHandler
-
 
 
 class Server:
@@ -19,19 +18,6 @@ class Server:
         self.socket.listen()
 
 
-    def generate_user(self, info):
-        return User(info[0], info[1], info[2], info[3], info[4])
-
-    def login(self, client: socket.socket):
-        client.send('login'.encode('utf-8'))
-        while True:
-            user_information = client.recv(4096).decode('utf-8')
-            info = user_information.split('//')
-            if self.client_handlers.search_by_username(info[0]) is not None:
-                user = self.generate_user(info)
-                client.send('done'.encode('utf-8'))
-            else:
-                client.send('try-again'.encode('utf-8'))
 
     def server_loop(self):
         while True:
